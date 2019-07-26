335f1c9230fbe16c524a3881748c2be0f6b7002a
rain <634778311@qq.com>
2019-07-26 18:19:13 +0800

    Update DDYQRCodeScanView.m

--- DDYQRCode/DDYQRCode/DDYQRCodeScanView.m
+++ DDYQRCode/DDYQRCode/DDYQRCodeScanView.m
@@ -19,6 +19,22 @@
 @end
 
 @implementation DDYQRCodeScanView
+@synthesize style = _style;
+@synthesize scanRectColor = _scanRectColor;
+
+- (DDYQRCodeScanViewStyle)style {
+    if (!_style) {
+        _style = DDYQRCodeScanViewStyleLine;
+    }
+    return _style;
+}
+
+- (UIColor *)scanRectColor {
+    if (!_scanRectColor) {
+        _scanRectColor = [UIColor colorWithRed:85./255. green:180./255. blue:55./255. alpha:1];
+    }
+    return _scanRectColor;
+}
 
 - (UIImageView *)scanBackView {
     if (!_scanBackView) {
@@ -29,18 +45,7 @@
 
 - (UIImageView *)scanLineView {
     if (!_scanLineView) {
-        CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-        basicAnimation.duration = 1.8;
-        basicAnimation.fromValue = 0;
-        basicAnimation.toValue = @(self.bounds.size.height);
-        basicAnimation.repeatCount = CGFLOAT_MAX;
-        basicAnimation.removedOnCompletion = NO;
-        basicAnimation.fillMode = kCAFillModeForwards;
-        basicAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
-        
-        
         _scanLineView = [[UIImageView alloc] init];
-        [_scanLineView.layer addAnimation:basicAnimation forKey:nil];
     }
     return _scanLineView;
 }
@@ -56,16 +61,42 @@
 
 - (void)setStyle:(DDYQRCodeScanViewStyle)style {
     _style = style;
-    if (style == DDYQRCodeScanViewStyleLine) {
-        self.scanLineView.frame = CGRectMake(0, -2, self.bounds.size.width, 2);
-        self.scanBackView.image = [DDYQRCodeManager scanImageWithColor:[UIColor colorWithRed:85./255. green:180./255. blue:55./255. alpha:1]];
-        self.scanLineView.image = [NSBundle ddyImage:@"ScanLine"];
+    [self changeStyle];
+}
+
+- (void)setScanRectColor:(UIColor *)scanRectColor {
+    _scanRectColor = scanRectColor;
+    [self changeStyle];
+}
+
+- (void)changeStyle {
+
+    if (self.style == DDYQRCodeScanViewStyleLine) {
+        self.scanLineView.frame = CGRectMake(0, 0, self.bounds.size.width, 2);
+        self.scanBackView.image = [DDYQRCodeManager scanImageWithColor:self.scanRectColor];
+        self.scanLineView.image = [[NSBundle ddyImage:@"ScanLine"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        self.scanLineView.tintColor = self.scanRectColor;
     } else {
-        self.scanLineView.frame = CGRectMake(0, -self.bounds.size.height, self.bounds.size.width, self.bounds.size.height);
-        self.scanBackView.image = [DDYQRCodeManager scanImageWithColor:[UIColor colorWithRed:255./255. green:128./255. blue:0./255. alpha:1]];
-        self.scanLineView.image = [NSBundle ddyImage:@"ScanGrid"];
+        self.scanLineView.frame = CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height);
+        self.scanBackView.image = [DDYQRCodeManager scanImageWithColor:self.scanRectColor];
+        self.scanLineView.image = [[NSBundle ddyImage:@"ScanGrid"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        self.scanLineView.tintColor = self.scanRectColor;
     }
-    
+    [self scanAnimation:-self.scanLineView.bounds.size.height/2.0 endValue:self.bounds.size.height+self.scanLineView.bounds.size.height/2.0];
+}
+
+- (void)scanAnimation:(CGFloat)startValue endValue:(CGFloat)endValue {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    basicAnimation.duration = 1.8;
+    basicAnimation.fromValue = @(startValue);
+    basicAnimation.toValue = @(endValue);
+    basicAnimation.repeatCount = CGFLOAT_MAX;
+    basicAnimation.removedOnCompletion = NO;
+    basicAnimation.fillMode = kCAFillModeForwards;
+    basicAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    [self.scanLineView.layer addAnimation:basicAnimation forKey:@"scanAnimation"];
 }
 
 @end
+// [DDYQRCodeManager scanImageWithColor:[UIColor colorWithRed:85./255. green:180./255. blue:55./255. alpha:1]]
+// [DDYQRCodeManager scanImageWithColor:[UIColor colorWithRed:255./255. green:128./255. blue:0./255. alpha:1]]
